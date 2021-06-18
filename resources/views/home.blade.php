1349b6f09ee9b23a18f607c9fcccc907aebcc39c
Polidora90 <alessandra.torry@gmail.com>
2021-06-18 16:51:58 +0200

    add scss files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
-<h1>Posso scrivere tutto l'html che voglio al posto del placeholder</h1>
+<div class="container">
+    <h1>Posso scrivere tutto l'html che voglio al posto del placeholder</h1>
+</div>
 
 @endsection
