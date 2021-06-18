706fdca299c56b5ef3293d6754611b12247c8b2d
Polidora90 <alessandra.torry@gmail.com>
2021-06-18 16:31:41 +0200

    add scss

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('page_title')</title>
+
+    {{-- css --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
