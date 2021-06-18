05a22cdf198c8f32fd389db6d0ccc6442ce9c2eb
Polidora90 <alessandra.torry@gmail.com>
2021-06-18 16:31:06 +0200

    add scss compile

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default')
+
+@section('page_title', 'Laravel Comix')
+
+@section('content')
+
+<h1>Posso scrivere tutto l'html che voglio al posto del placeholder</h1>
+
+@endsection
