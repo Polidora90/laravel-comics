a962be1b4144d577eb6ef8e387b54dcac8f896f3
Polidora90 <alessandra.torry@gmail.com>
2021-06-21 20:27:50 +0200

    add comics info

--- resources/views/singleProduct.blade.php
+++ resources/views/singleProduct.blade.php
@@ -20,6 +20,21 @@
     <section class="main-info">
         <div class="container">
             <h2>{{ strtoupper($comic['title']) }}</h2>
+
+            <div class="price-container flex">
+                <div class="price flex">
+                    <h4>U.S. Price: {{ $comic['price'] }}</h4>
+                    <h4>AVAILABLE</h4>
+                </div>
+                <div class="availability">
+                    <h4>Chech Availability <i class="fas fa-sort-down"></i></h4>
+                </div>
+            </div>
+
+            <div>
+                <p>{{ $comic['description'] }}</p>
+            </div>
+           
         </div>
     </section>
        
