61b7d11f99d34c6805b3a03e76a7f6e7a52046b2
Polidora90 <alessandra.torry@gmail.com>
2021-06-21 14:22:05 +0200

    corregge posizionamento dei file

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,60 @@
+<footer>
+    <section class="contacts">
+        <div class="container">
+            <div class="row flex">
+                <div class="col-2">
+                    <ul>
+                        <li>DC COMICS</li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <ul>
+                        <li>SHOP</li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-2">
+                    <ul>
+                        <li>DC</li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (News)</a></li>
+                        <li><a href="#">Ad Choice</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-2">
+                    <ul>
+                        <li>SITES</li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <p>All Site Content TN and <i class="far fa-copyright"></i> 2020 DC Enrertainment, unless uotherwise <a href="#">noted here</a>. All right reserved.</p>
+            <a href="#">Cookies Settings</a>
+        </div>
+    </section>
+
+    <section class="socials">
+    </section>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,35 @@
+<header>
+        {{-- banner blu --}}
+    <div class="nav-banner">
+        <div class="container flex">
+            <a href="#">DC POWER VISA <i class="far fa-registered"></i></a>
+            <a href="#">ADDITIONAL DC SITES <i class="fas fa-sort-down"></i></a>
+        </div>
+    </div>
+    {{-- navbar --}}
+    <nav>
+        <div class="container flex">
+            <div class="logo-container">
+                <img class="responsive-img" src="{{ asset('img/dc-logo.png')}}" alt="">
+            </div>
+
+            <ul class="flex">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTEBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP <span><i class="fas fa-sort-down"></i></span></a></li>
+            </ul>
+            <input type="text" placeholder="Search">
+        </div>
+    </nav>
+
+    <section class="jumbotron">
+
+    </section>
+</header>
